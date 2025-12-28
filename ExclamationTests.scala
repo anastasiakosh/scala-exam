781e36e89d02129763d75abfc19459bb3bee9d8d
anastasiakosh <anastasiakoseleva31@hg47.org>
2025-12-28 19:51:20 +0200

    Add exclamation marks solution and tests

---
+++ ExclamationTests.scala
@@ -0,0 +1,17 @@
+class RemoveSpec extends org.scalatest.flatspec.AnyFlatSpec {
+  "remove" should "pass basic tests" in {
+    val tests = Seq(
+      ("Hi!" , "Hi!"),
+      ("Hi!!!" ,"Hi!"),
+      ("!Hi" , "Hi!"),
+      ("!Hi!" , "Hi!"),
+      ("Hi! Hi!" , "Hi Hi!"),
+      ("Hi" , "Hi!"),
+    )
+    
+    tests foreach {
+      (s, expected) =>
+        assert(remove(s) == expected, s"for remove(\"$s\")")
+    }
+  }
+}
